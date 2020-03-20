6152192d7678f141c1da5a944996a1d801177c57
Luca Iaconelli <lucabelli@msn.com>
2020-03-20 18:18:48 +0100

    chore: updated github page

--- test/module.test.js
+++ test/module.test.js
@@ -1,7 +1,6 @@
 import config from '../example/nuxt.config';
 
 import { Nuxt, Builder } from 'nuxt';
-import { JSDOM } from 'jsdom';
 import request from 'request-promise-native';
 import getPort from 'get-port';
 
@@ -19,23 +18,10 @@ let nuxt
 ;
 
 const url = path => `http://localhost:${ port }${ path }`
-    , headers = ua => (
-        ! ua
-        ? {}
-        : {
-            'User-Agent': ua,
-        }
-    )
-    , get = (
-        path,
-        ua
-    ) => request(
+    , get = path => request(
         {
             url: url(
                 path
-            ),
-            headers: headers(
-                ua
             ),
         }
     )
@@ -86,118 +72,7 @@ describe(
                 expect(
                     html
                 ).toContain(
-                    'Nuxt'
-                );
-
-            }
-        );
-
-        const getElements = async(
-            selector,
-            ua
-        ) => {
-
-            const html = await get(
-                    BASE_URL,
-                    ua
-                )
-                , { window } = new JSDOM(
-                    html
-                ).window
-                , elements = window.document.querySelectorAll(
-                    selector
-                )
-            ;
-
-            return elements.length;
-
-        };
-
-        describe(
-            'website',
-            () => {
-
-                test(
-                    'scripts',
-                    async() => {
-
-                        const elements = await getElements(
-                            'link[rel="preload"][as="script"]'
-                        );
-
-                        // Test
-                        expect(
-                            elements
-                        ).toBeGreaterThanOrEqual(
-                            0
-                        );
-
-                    }
-                );
-
-                test(
-                    'scripts',
-                    async() => {
-
-                        const elements = await getElements(
-                            'script:not([type="application/ld+json"])',
-                        );
-
-                        // Test
-                        expect(
-                            elements
-                        ).toBeGreaterThanOrEqual(
-                            0
-                        );
-
-                    }
-                );
-
-            }
-        );
-
-        describe(
-            'is-bot',
-            () => {
-
-                const USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)';
-
-                test(
-                    'scripts',
-                    async() => {
-
-                        const elements = await getElements(
-                            'link[rel="preload"][as="script"]',
-                            USER_AGENT
-                        );
-
-                        // Test
-                        expect(
-                            elements
-                        ).toEqual(
-                            0
-                        );
-
-                    }
-                );
-
-                test(
-                    'scripts',
-                    async() => {
-
-                        const elements = await getElements(
-                            'script:not([type="application/ld+json"])',
-                            USER_AGENT
-                        );
-
-                        // Test
-                        expect(
-                            elements
-                        ).toEqual(
-                            0
-                        );
-
-                    }
+                    'Generative'
                 );
 
             }

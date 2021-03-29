9b7e6c99713b9c174e457203d2845331e009fe0f
Luca Iaconelli <lucabelli@msn.com>
2021-03-29 22:57:20 +0200

    docs: updated github pages

--- docs/_nuxt/static/1616453370/experiments/tensorflow-facemesh/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgenerative-art\u002F_nuxt\u002Fstatic\u002F1616453370",layout:"default",error:a,serverRendered:true,routePath:"\u002Fexperiments\u002Ftensorflow-facemesh",config:{googleAnalytics:{id:"UA-56136653-8"},app:{basePath:"\u002Fgenerative-art\u002F",assetsPath:"\u002Fgenerative-art\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ docs/_nuxt/static/1617051418/single/page-6/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgenerative-art\u002F_nuxt\u002Fstatic\u002F1617051418",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsingle\u002Fpage-6",config:{googleAnalytics:{id:"UA-56136653-8"},app:{basePath:"\u002Fgenerative-art\u002F",assetsPath:"\u002Fgenerative-art\u002F_nuxt\u002F",cdnURL:a}}}}(null));
